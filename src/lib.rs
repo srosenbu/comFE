0a9c1135f2ea830eb194db1adde1b691f97758f0
srosenbu <sjard-mathis.rosenbusch@bam.de>
2025-07-02 09:09:43 +0200

    infer tangent size ffrom Stress_strain size

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ use crate::interfaces::*;
 use crate::linear_elasticity::*;
 use crate::mises_plasticity::*;
 use crate::mandel::*;
-use crate::utils::*;
 //use crate::cmap::*;
 
 pub mod consts;
@@ -11,5 +10,4 @@ pub mod interfaces;
 pub mod linear_elasticity;
 pub mod mises_plasticity;
 pub mod mandel;
-pub mod utils;
 //pub mod cmap;
